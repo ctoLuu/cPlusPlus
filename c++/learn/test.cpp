4409e3a4559be9cbb2818a0ad0c4d9ae522d6e20
stoair <2296663099@qq.com>
2024-03-22 18:45:12 +0800

    c

--- c++/learn/test.cpp
+++ c++/learn/test.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<string>
 #include<chrono>
+#include<cstring>
 using namespace std;
 
 /*int main()
@@ -51,3 +52,82 @@ using namespace std;
 //     obj2.i = 3;
 //     cout << obj.i << " " << obj2.i <<endl; 
 // } 
+
+// class Apple 
+// { 
+//     public: 
+//         static int i;
+//         // static member function 
+//         static void printMsg() 
+//         {
+//             cout<<"Welcome to Apple!" << i; 
+//         }
+// }; 
+// int Apple::i = 1;
+
+// // main function 
+// int main() 
+// { 
+//     // invoking a static member function 
+//     Apple::printMsg(); 
+// } 
+
+// class Circle {
+// private:
+//     double radius;
+
+// public:
+//     Circle(double r) : radius(r) {}
+
+//     double getArea() const {
+//         return 3.14 * radius * radius;
+//     }
+// };
+
+// int main() {
+//     Circle c(5); // 常对象c，半径为5
+
+//     // 调用常成员函数getArea计算常对象c的面积
+//     double area = c.getArea();
+
+//     std::cout << "The area of the circle with radius 5 is: " << area << std::endl;
+
+//     return 0;
+// }
+
+class Person{
+public:
+    typedef enum {
+        BOY = 0, 
+        GIRL 
+    }SexType;
+    Person(const char *n, int a,SexType s){
+        name=new char[strlen(n)+1];
+        strcpy(name,n);
+        age=a;
+        sex=s;
+    }
+    int get_age() const{
+
+        return this->age; 
+    }
+    Person& add_age(int a){
+        age+=a;
+        return *this; 
+    }
+    ~Person(){
+        delete [] name;
+    }
+private:
+    char * name;
+    int age;
+    SexType sex;
+};
+
+
+int main(){
+    Person p("zhangsan",20,Person::BOY); 
+    cout<<p.get_age()<<endl;
+    cout<<p.add_age(10).get_age()<<endl;
+    return 0;
+}
