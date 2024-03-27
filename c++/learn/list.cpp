fb38de7e7d1257f5ead07ce06c013495ec3a52f5
stoair <2296663099@qq.com>
2024-03-27 18:19:35 +0800

    sortList

--- c++/learn/list.cpp
+++ c++/learn/list.cpp
@@ -1,28 +1,77 @@
 #include<iostream>
 #include<list>
+#include<algorithm>
 using namespace std;
+
+// int main()
+// {
+//     list<int> lst;
+//     int x;
+//     while (cin >> x)
+//     {
+//         if (x > 0)
+//             lst.push_front(x);
+//         else
+//             lst.push_back(x);
+//         if (cin.get() == '\n')
+//             break;
+//     }
+//     cout << "size = " << lst.size() << endl;
+//     cout << "front = " << lst.front() << endl;
+//     cout << "back = " << lst.back() << endl;
+//     list<int>::iterator it;
+//     for (it = lst.begin(); it != lst.end(); it++)
+//     {
+//         cout << *it << '\t';
+//     } 
+//     cout << endl;
+//     return 0;
+// }
 
 int main()
 {
-    list<int> lst;
-    int x;
-    while (cin >> x)
-    {
-        if (x > 0)
-            lst.push_front(x);
-        else
-            lst.push_back(x);
-        if (cin.get() == '\n')
-            break;
-    }
-    cout << "size = " << lst.size() << endl;
-    cout << "front = " << lst.front() << endl;
-    cout << "back = " << lst.back() << endl;
+    list<int> L1, L2, L3;
     list<int>::iterator it;
-    for (it = lst.begin(); it != lst.end(); it++)
+    int x;
+    while (cin >> x && x != 0)
     {
-        cout << *it << '\t';
-    } 
+        if (x % 2 == 0){;
+            L2.push_back(x);
+            L3.push_back(x);
+        }
+        else{
+            L1.push_back(x);
+            L3.push_back(x);
+        }
+    }
+    L1.sort();
+    L2.sort();
+    L3.sort();
+    it = L1.begin();
+    for (size_t i = 0; i < L1.size(); i++)
+    {
+        cout << *it;
+        it++;
+        if (it != L1.end())
+            cout << "->";
+    }
     cout << endl;
-    return 0;
+    it = L2.begin();
+    for (size_t i = 0; i < L2.size(); i++)
+    {
+        cout << *it;
+        it++;
+        if (it != L2.end())
+            cout << "->";
+    }
+    cout << endl;
+    it =  L3.begin();
+    for (size_t i = 0; i < L3.size(); i++)
+    {
+        cout << *it;
+        it++;
+        if (it != L3.end())
+            cout << "->";
+    }
+    cout << endl;
 }
--- c++/learn/test.cpp
+++ c++/learn/test.cpp
@@ -2,6 +2,10 @@
 #include<string>
 #include<chrono>
 #include<cstring>
+#include<list>
+#include<deque>
+#include<algorithm>
+#define pi 3.1415926
 using namespace std;
 
 /*int main()
@@ -95,39 +99,177 @@ using namespace std;
 //     return 0;
 // }
 
-class Person{
-public:
-    typedef enum {
-        BOY = 0, 
-        GIRL 
-    }SexType;
-    Person(const char *n, int a,SexType s){
-        name=new char[strlen(n)+1];
-        strcpy(name,n);
-        age=a;
-        sex=s;
-    }
-    int get_age() const{
+// class Person{
+// public:
+//     typedef enum {
+//         BOY = 0, 
+//         GIRL 
+//     }SexType;
+//     Person(const char *n, int a,SexType s){
+//         name=new char[strlen(n)+1];
+//         strcpy(name,n);
+//         age=a;
+//         sex=s;
+//     }
+//     int get_age() const{
 
-        return this->age; 
-    }
-    Person& add_age(int a){
-        age+=a;
-        return *this; 
-    }
-    ~Person(){
-        delete [] name;
-    }
-private:
-    char * name;
-    int age;
-    SexType sex;
-};
+//         return this->age; 
+//     }
+//     Person& add_age(int a){
+//         age+=a;
+//         return *this; 
+//     }
+//     ~Person(){
+//         delete [] name;
+//     }
+// private:
+//     char * name;
+//     int age;
+//     SexType sex;
+// };
 
 
-int main(){
-    Person p("zhangsan",20,Person::BOY); 
-    cout<<p.get_age()<<endl;
-    cout<<p.add_age(10).get_age()<<endl;
+// int main(){
+//     Person p("zhangsan",20,Person::BOY); 
+//     cout<<p.get_age()<<endl;
+//     cout<<p.add_age(10).get_age()<<endl;
+//     return 0;
+// }
+
+// void Print_D(int x)
+// {
+// 	cout.width(5);			
+// 	cout<<x;
+// }
+// int main()
+// {
+//     list<int> L;
+//     int x;
+//     while (cin >> x && x != 0){
+//         if (x > 0)
+//             L.push_front(x);
+//         else
+//             L.push_back(x);
+//     }
+//     for_each(L.begin(),L.end(),Print_D);
+//     deque<int> D;
+//     while (cin >> x && x != 0){
+//         if (x > 0)
+//             D.push_front(x);
+//         else
+//             D.push_back(x);
+//     }
+//     for_each(D.begin(),D.end(),Print_D);
+// }
+// #include <iomanip>
+
+// class Circle
+// {
+// public:
+//     Circle(float r) : R(r) {}
+//     float S();
+//     float C();
+// private:
+//     float R;
+// };
+
+// float Circle::S()
+// {
+//     float S = pi * R * R;
+//     return S;
+// }
+
+// float Circle::C()
+// {
+//     float C = 2 * pi * R;
+//     return C;
+// }
+
+// int main()
+// {
+//     float r;
+//     std::cin >> r;
+//     Circle c(r);
+//     std::cout << std::fixed << std::setprecision(2) << c.S()*20 + c.C()*35 << std::endl;
+//     return 0;
+// }
+
+#include<iostream>
+
+using namespace std;
+typedef struct node{
+    int num;
+    struct node* next;
+}Node;
+
+Node* listcreate(int M)
+{
+    int i = 1;
+    Node* head;
+    head = new Node;
+    head->num  = i;
+    head->next = NULL;
+    Node* current = head;
+    for (i = 2; i <= M + 1; i++)
+    {
+        if (i != M + 1){
+            current->next = new Node;
+            current = current->next;
+            current->num = i;
+            current->next = NULL;
+        }
+        else{
+            current->next = head;
+            break;
+        }
+    }
+    return head;
+}
+
+void listOut(Node* head, int M, int N)
+{
+    Node* current = head;
+    Node* pre = head;
+    Node* kill = NULL;
+    while (pre->next != head)
+        pre = pre->next;
+    for (int i = 0; i < M; i++)
+    {
+        if (i == M-2){
+            for (int j = 1; j < N; j++)
+            {
+                pre = pre->next;
+                current = current->next;
+            }
+            cout.width(4);
+            cout << current->num;
+            cout.width(4);
+            cout << pre->num;
+            delete current;
+            delete pre;
+            break;
+        }
+        for (int j = 1; j < N; j++)
+        {
+            pre = pre->next;
+            current = current->next;
+        }
+        kill = new Node;
+        kill = current;
+        cout.width(4);
+        cout << current->num;
+        current = current->next;
+        pre->next = current;
+        delete kill;
+    }
+}
+
+int main()
+{
+    int M,N;
+    cin >> M;
+    cin >> N;
+    Node *head = listcreate(M);
+    listOut(head, M, N);
     return 0;
 }
